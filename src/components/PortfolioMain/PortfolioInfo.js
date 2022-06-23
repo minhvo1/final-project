604c499a15216923f38d8df328f9c3becede29d9
Minh <minhvo199@gmail.com>
2022-06-23 14:18:02 -0600

    add portfolio value and return

--- src/components/PortfolioMain/PortfolioInfo.js
+++ src/components/PortfolioMain/PortfolioInfo.js
@@ -3,7 +3,22 @@ import React from 'react';
 export default function PortfolioInfo(props) {
   return (
     <div className="portfolio-info">
-      
+      <div className="portfolio-value-return">
+        <div className="portfolio-value">
+          <header>Portfolio Value</header>
+          <span>$100,000</span>
+        </div>
+        <div className="portfolio-return">
+          <header>Total Return</header>
+          <span>+ $5,000</span>
+        </div>
+      </div>
+      <div className="daily-return">
+
+      </div>
+      <div className="available-funds">
+        
+      </div>
     </div>
-  )
+  );
 }
