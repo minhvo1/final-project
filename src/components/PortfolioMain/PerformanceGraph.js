1ed51fd379afac7f283bcb8e77603031e0342a0a
Minh <minhvo199@gmail.com>
2022-06-23 20:08:33 -0600

    implement graph headers

--- src/components/PortfolioMain/PerformanceGraph.js
+++ src/components/PortfolioMain/PerformanceGraph.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { Chart } from 'chart.js'
 
 export default function PerformanceGraph(props) {
+  const timeframes = ['1D','1W','1M','3M','1Y','All'];
   return (
     <div className="performance-graph">
-      
+      <div>
+        <span>Performance Graph</span>
+        <div className="timeframe-buttons">
+          {timeframes.map((time) => {
+            return (
+              <button>{time}</button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="main-graph">
+
+      </div>
     </div>
   )
 }
