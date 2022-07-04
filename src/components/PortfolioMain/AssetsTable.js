492c167ff7332401f1b2011a3a20f3bc92ae02bd
Minh <minhvo199@gmail.com>
2022-07-03 23:12:58 -0600

    create asset table

--- src/components/PortfolioMain/AssetsTable.js
+++ src/components/PortfolioMain/AssetsTable.js
@@ -3,7 +3,30 @@ import React from 'react';
 export default function AssetTable(props) {
   return (
     <div className="asset-table">
-      
+      <div>
+        <span>Assets</span>
+      </div>
+
+      <div>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+            <th>Quanity</th>
+            <th>Amount</th>
+            <th>Return</th>
+          </tr>
+          <tr>
+            <td>GameStop</td>
+            <td>GME</td>
+            <td>$100,000</td>
+            <td>$10,000</td>
+            <td>5,000,000</td>
+            <td>+ $5,000</td>
+          </tr>
+        </table>
+      </div>
     </div>
   )
 }
