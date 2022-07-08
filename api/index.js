e5b2eb0c52d5bff6458078f3bffa359b6eb22018
ttbinh139 <ttbinh139@gmail.com>
2022-07-08 17:13:46 +0000

    Get all tickers and generate insert query into seeds.sql

--- api/index.js
+++ api/index.js
@@ -1,23 +1,22 @@
 require("dotenv").config();
 
-
 const express = require("express");
 const bodyparser = require("body-parser");
-const request = require('request');
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 // PG database client/connection setup
-//const {StringStream} = require("scramjet");
+/* const csv = require('csv-parser')
+const fs = require('fs') */
+
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
 const { json } = require("express");
 const db = new Pool(dbParams);
-db.connect();
+//db.connect();
 
-console.log(db);
+//console.log(db);
 
 app.get("/", (req, res) => {
   db.query(`SELECT * FROM tickers`)
@@ -25,15 +24,26 @@ app.get("/", (req, res) => {
       console.log(data);
       res.json(data.rows);
     })
-    .catch(err => res.json({message: err}));
+    .catch(err => res.json({ message: err }));
 });
 
 /* app.get("/get_tickers", (req, res) => {
-  return request.get("https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=2DHC1EFVR3EOQ33Z")
-    .pipe(new StringStream())
-    .CSVParse()                                   // parse CSV output into row objects
-    .consume(object => console.log("Row:", object))
-    .then(() => console.log("success"));
+  const results = [];
+
+  fs.createReadStream('./datas/listing_status.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      //console.log(results);
+      let string = "";
+      for (const row of results) {
+        string += `INSERT INTO tickers (portfolio_id, ticker, company_name) VALUES (1, '${row['symbol']}', '${row['name']}');`
+        string += "<br />";
+    
+      }
+      console.log(string);
+      return res.send(string);
+    });
 }); */
 
 app.listen(PORT, () => {
